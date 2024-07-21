818ce97772bd90679be10103b17981d583aa9951
Muhkiddin <muhiddinsaidturaev63@gmail.com>
2024-07-21 19:57:52 +0500

    7 oy 1 dars uy ishi

---
+++ src/pages/note-found/note-found.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+ export const NoteFound = () => {
+  return (
+    <div>
+       <div className=" container rounded-md bg-[#ffffff] flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl text-primary font-extrabold tracking-wider mb-8 animate-bounce">
+        Page Not Found !!
+      </h1>
+      <img
+        className="w-1/2"
+        src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
+        alt="not found"
+      />
+    </div>
+    </div>
+  )
+}
+
+
