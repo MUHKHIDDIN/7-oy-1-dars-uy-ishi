90abc3448feb327019f4d69f950722e47e949953
Muhkiddin <muhiddinsaidturaev63@gmail.com>
2024-07-21 20:27:29 +0500

    new

--- src/pages/note-found/note-found.jsx
+++ src/pages/note-found/note-found.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 
- export const NoteFound = () => {
+export const NoteFound = () => {
   return (
-    <div>
-       <div className=" container rounded-md bg-[#ffffff] flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl text-primary font-extrabold tracking-wider mb-8 animate-bounce">
-        Page Not Found !!
-      </h1>
-      <img
-        className="w-1/2"
-        src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
-        alt="not found"
-      />
+    <div className="container pb-[80px]">
+      <div className="container flex h-screen flex-col items-center justify-center rounded-md bg-[#ffffff]">
+        <h1 className="text-primary mb-8 animate-bounce text-6xl font-extrabold tracking-wider">
+          Page Not Found !!
+        </h1>
+        <img
+          className="w-1/2"
+          src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
+          alt="not found"
+        />
+      </div>
     </div>
-    </div>
-  )
-}
-
-
+  );
+};
